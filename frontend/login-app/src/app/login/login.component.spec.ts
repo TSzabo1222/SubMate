da50a7cef5d6c57ae0354dc9cee556269ee37df2
TSz1222 <szabo.timea@dozsakalocsa.hu>
2026-04-20 12:29:33 +0200

    fix: login teszt hiba

--- frontend/login-app/src/app/login/login.component.spec.ts
+++ frontend/login-app/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { LoginComponent } from './login.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,19 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [],
+      providers:[ {
+
+        provide:{
+          succes:()=>{},
+          error:()=>{},
+
+        }
+      },
+    provideHttpClient(),
+  provideHttpClientTesting()
+]
     })
     .compileComponents();
 
